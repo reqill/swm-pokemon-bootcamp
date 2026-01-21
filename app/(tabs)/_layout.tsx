abc04b068b4d81ee35e62e2adc76f7e344612218
Mikołaj Mrózek <31847808+reqill@users.noreply.github.com>
2026-01-21 10:01:13 +0100

    feat: navigation structure (#1)

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -20,9 +20,9 @@ export default function TabLayout() {
       <Tabs.Screen
         name="index"
         options={{
-          title: "Home",
+          title: "Favorite",
           tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="house.fill" color={color} />
+            <IconSymbol size={28} name="star.fill" color={color} />
           ),
         }}
       />
@@ -31,7 +31,25 @@ export default function TabLayout() {
         options={{
           title: "Explore",
           tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="paperplane.fill" color={color} />
+            <IconSymbol size={28} name="magnifyingglass" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="camera"
+        options={{
+          title: "Camera",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="camera.fill" color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="map"
+        options={{
+          title: "Map",
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name="map.fill" color={color} />
           ),
         }}
       />
---
+++ app/(tabs)/camera.tsx
@@ -0,0 +1,33 @@
+import ParallaxScrollView from "@/components/parallax-scroll-view";
+import { ThemedText } from "@/components/themed-text";
+import { ThemedView } from "@/components/themed-view";
+import { Fonts } from "@/constants/theme";
+import { StyleSheet } from "react-native";
+
+export default function CameraScreen() {
+  return (
+    <ParallaxScrollView
+      headerBackgroundColor={{ light: "#dcbba1", dark: "#47331d" }}
+      headerImage={<></>}
+    >
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText
+          type="title"
+          style={{
+            fontFamily: Fonts.rounded,
+          }}
+        >
+          Camera
+        </ThemedText>
+      </ThemedView>
+    </ParallaxScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: "row",
+    alignItems: "center",
+    gap: 8,
+  },
+});
