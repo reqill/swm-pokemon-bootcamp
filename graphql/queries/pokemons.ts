4c91fdbdcaa5b218076728ced0046663d00e121f
Mikołaj Mrózek <mikolaj.mrozek@swmansion.com>
2026-01-21 12:18:23 +0100

    chore: test signer email valididty

--- graphql/queries/pokemons.ts
+++ graphql/queries/pokemons.ts
@@ -2,15 +2,9 @@ import { gql } from '../types';
 
 export const GET_POKEMONS = gql(`
   query getPokemons($limit: Int!) {
-    pokemons: pokemon(
-      limit: $limit, 
-      order_by: { name: asc }, 
-      where: { 
-        is_default:  {
-          _eq: true
-        }
-      }
-    ) {
+    pokemons: pokemon(limit: $limit, order_by: { name: asc }, where: { is_default:  {
+       _eq: true
+    }}) {
       id
       name
     }
