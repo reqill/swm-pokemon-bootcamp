50f6bbf7dfb610ec619b39e730e0e25d828ec3dd
Mikołaj Mrózek <mikolaj.mrozek@swmansion.com>
2026-01-23 18:09:54 +0100

    feat: camera display with basic face tracking

--- app/(tabs)/camera.tsx
+++ app/(tabs)/camera.tsx
@@ -1,30 +1,70 @@
-import ParallaxScrollView from '@/components/parallax-scroll-view';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
-import { Fonts } from '@/constants/theme';
-import { StyleSheet } from 'react-native';
+import { useIsFocused } from '@react-navigation/native';
+import { useEffect, useRef } from 'react';
+import { Dimensions, StyleSheet } from 'react-native';
+import { Camera, useCameraDevice, useCameraPermission, useFrameProcessor } from 'react-native-vision-camera';
+import { FrameFaceDetectionOptions, useFaceDetector } from 'react-native-vision-camera-face-detector';
 
 export default function CameraScreen() {
-  return (
-    <ParallaxScrollView headerBackgroundColor={{ light: '#dcbba1', dark: '#47331d' }} headerImage={<></>}>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText
-          type="title"
-          style={{
-            fontFamily: Fonts.rounded,
-          }}
-        >
-          Camera
-        </ThemedText>
+  const isFocused = useIsFocused();
+  const { hasPermission, requestPermission } = useCameraPermission();
+
+  const faceDetectionOptions = useRef<FrameFaceDetectionOptions>({
+    autoMode: true,
+    classificationMode: 'none',
+    windowHeight: Dimensions.get('window').height,
+    windowWidth: Dimensions.get('window').width,
+    performanceMode: 'fast',
+    landmarkMode: 'none',
+  }).current;
+
+  const device = useCameraDevice('front');
+  const { detectFaces, stopListeners } = useFaceDetector(faceDetectionOptions);
+
+  useEffect(() => {
+    return () => stopListeners();
+  }, []);
+
+  useEffect(() => {
+    if (isFocused && !hasPermission) requestPermission();
+  }, [hasPermission, requestPermission, isFocused]);
+
+  const frameProcessor = useFrameProcessor((frame) => {
+    'worklet';
+
+    const faces = detectFaces(frame);
+
+    if (faces.length > 0) {
+      console.log(`Detected ${JSON.stringify(faces)}`);
+    }
+  }, []);
+
+  if (!device || !hasPermission) {
+    return (
+      <ThemedView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <ThemedText>Requesting Camera Access...</ThemedText>
       </ThemedView>
-    </ParallaxScrollView>
+    );
+  }
+
+  return (
+    <Camera
+      style={StyleSheet.absoluteFill}
+      device={device}
+      isActive={isFocused}
+      frameProcessor={frameProcessor}
+      pixelFormat="yuv"
+    />
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
+  pokemonSprite: {
+    position: 'absolute',
+    width: 100,
+    height: 100,
+    top: 0,
+    left: 0,
   },
 });
